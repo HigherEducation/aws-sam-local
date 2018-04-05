b05be8d7b345485058f66012ebc7df81ba5a804a
Ky Phan <kphan@highereducation.com>
2018-04-05 08:43:45 -0700

    Made python-with-packages sample compatible with python2.7
    We're only supporting python2.7 for now.

--- samples/python-with-packages/example/schedule.py
+++ samples/python-with-packages/example/schedule.py
@@ -4,6 +4,5 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-def lambda_sandbox_down_handler(event: dict, context):
+def lambda_sandbox_down_handler(event, context):
     log.info('running DOWN handler with event: %s and context: %s', event, context)
-
